4ba45153697bd7be1021eecfda3a5fd101a2f2e6
Gabriel Salvador Palmiciano <salva@morean.co>
2023-07-23 14:16:31 -0300

    initial commit lita assitant

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config)
+ 
+
+export const runtime = 'edge'
+ 
+export async function POST(req: Request) {
+  const { messages } = await req.json()
+ 
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages,
+    max_tokens: 100,
+    temperature: 0,
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1
+  })
+
+  const stream = OpenAIStream(response)
+
+  return new StreamingTextResponse(stream)
+}
---
+++ src/components/Chat.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { useChat } from 'ai/react';
+
+import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { ScrollArea } from "./ui/scroll-area";
+
+export interface ChatProps {}
+
+export function Chat() {
+  const{ messages, input, handleInputChange, handleSubmit } = useChat({
+    api: '/api/chat',
+  })
+
+  return (  
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle>LITA</CardTitle>
+          <CardDescription>Tu asistente financiero</CardDescription>
+        </CardHeader>
+          <CardContent>
+            <ScrollArea className="h-[600px] w-full space-y-4 pr-4">          
+              { messages.map(message => {
+                return (            
+                  <div key={message.id} className="flex gap-2 text-slate-600 text-sm mb-4">
+                    {message.role === 'user' &&(
+                      <Avatar>
+                        <AvatarFallback>US</AvatarFallback>
+                        <AvatarImage src="https://api.multiavatar.com/Satoshi Bond.png" />
+                      </Avatar>
+                    )}
+
+                    {message.role === 'assistant' &&(
+                      <Avatar>
+                        <AvatarFallback>AS</AvatarFallback>
+                        <AvatarImage src="https://api.multiavatar.com/ Bond.png" />
+                      </Avatar>
+                    )}
+
+                    <p className="landing-relaxed">
+                      <span className="block font-bold text-slate-800">
+                        {message.role === 'user' ? 'Usuario' : 'LITA'}:
+                      </span>
+                      {message.content}
+                    </p>
+                  </div> 
+                )
+              }) }
+            </ScrollArea>
+          </CardContent>
+          <CardFooter >
+            <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+              <Input type="text" placeholder="¿Cómo puedo ayudarte?" value={input} onChange={handleInputChange} />
+              <Button type="submit">Enviar</Button>
+            </form>
+          </CardFooter>
+      </Card>
+  )
+}
